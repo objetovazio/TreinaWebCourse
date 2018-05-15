35aa38db71f22cb7fc85950e04c074235f5d14b0
André Martins <objetovazio@gmail.com>
2018-05-14 22:01:34 -0300

    update bootstrap

--- TreinaWeb.ProjetoTeste/TreinaWeb.ProjetoTeste.Web/Models/Pessoa.cs
+++ TreinaWeb.ProjetoTeste/TreinaWeb.ProjetoTeste.Web/Models/Pessoa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,16 @@ namespace TreinaWeb.ProjetoTeste.Web.Models
 {
     public class Pessoa
     {
+        [Required]
+        [MinLength(5, ErrorMessage = "Use no mínimo 5 letras.")]
         public string Nome { get; set; }
+
+        [Required]
+        [Range(18, int.MaxValue, ErrorMessage = "Apenas pessoas maiores de idade podem cadastrar-se.")]
         public int Idade { get; set; }
+
+        [Required]
+        [MaxLength(200, ErrorMessage = "Máximo de caracteres 200.")]
         public String Endereco { get; set; }
     }
 }
